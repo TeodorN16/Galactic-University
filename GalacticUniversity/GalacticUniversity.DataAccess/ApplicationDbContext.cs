f345e6e227775e3c655c4c9b23253a69f70cd657
Teodor <nedelchevm.tm@gmail.com>
2025-01-05 17:36:56 +0000

    .

--- GalacticUniversity/GalacticUniversity.DataAccess/ApplicationDbContext.cs
+++ GalacticUniversity/GalacticUniversity.DataAccess/ApplicationDbContext.cs
@@ -12,6 +12,12 @@ namespace GalacticUniversity.DataAccess
     public class ApplicationDbContext:DbContext
     {
         public ApplicationDbContext(DbContextOptions options): base(options) { }
+
         public DbSet<User> users { get; set; }
+        public DbSet<LectureResource> lectureResources { get; set; }
+        public DbSet<Lecture> lectures { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<Comment> comments { get; set; }
+        public DbSet<Category> categories { get; set; }
     }
 }
