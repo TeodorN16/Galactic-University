9f2a9f4abc353d00c9829bd2585f44ba09febfc7
Teodor <nedelchevm.tm@gmail.com>
2025-01-07 22:24:28 +0000

    Started Adding Repositories to all Models

---
+++ GalacticUniversity/GalacticUniversity.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticUniversity.Core.Repository
+{
+    public interface IRepository<T> where T:class
+    {
+        public void Add(T obj);
+
+        public void Delete(T obj);
+
+        public void Update(T obj);
+
+        public List<T> GetAll();
+        
+    }
+}
