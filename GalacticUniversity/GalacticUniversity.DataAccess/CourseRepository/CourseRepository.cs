e7c141a1a7694f6315630b56ec29f1b0532bc5ac
TeodorN16 <149152387+TeodorN16@users.noreply.github.com>
2025-01-08 15:33:17 +0200

    Completed All Repositories

--- GalacticUniversity/GalacticUniversity.DataAccess/CourseRepository/CourseRepository.cs
+++ GalacticUniversity/GalacticUniversity.DataAccess/CourseRepository/CourseRepository.cs
@@ -1,4 +1,5 @@
 using GalacticUniversity.Core.Repository;
+using GalacticUniversity.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +8,20 @@ using System.Threading.Tasks;
 
 namespace GalacticUniversity.DataAccess.CourseRepository
 {
-    public class CourseRepository : <LectureResource>
+    public class CourseRepository : Repository<Course>
     {
         private readonly ApplicationDbContext _context;
         public CourseRepository(ApplicationDbContext context) : base(context)
         {
             _context = context;
         }
+        public ICollection<Course> GetCourseByCategory(string category)
+        {
+            return _context.courses
+                           .Where(c => c.Category.CategoryName== category) // ????????? ????????? ?? ?????????
+                           .ToList(); // ???????????? ????????? ? ??????
+        }
+
 
     }
 }
