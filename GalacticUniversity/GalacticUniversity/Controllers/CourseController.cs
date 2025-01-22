69a2c66f63d71f4e83bf266aefb01b7b17e4a05e
TeodorN16 <149152387+TeodorN16@users.noreply.github.com>
2025-01-22 15:40:02 +0200

    .

--- GalacticUniversity/GalacticUniversity/Controllers/CourseController.cs
+++ GalacticUniversity/GalacticUniversity/Controllers/CourseController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GalacticUniversity.Core.CourseService;
+using GalacticUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GalacticUniversity.Controllers
 {
     public class CourseController : Controller
     {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        { 
+            _courseService = courseService; 
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Course course)
+        { 
+            _courseService.Add(course);
+            return RedirectToAction("Index");
         }
     }
 }
