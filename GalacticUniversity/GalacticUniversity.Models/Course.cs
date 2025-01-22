33edaf2088f3b5bae42340f5a81ae800516d2801
TeodorN16 <149152387+TeodorN16@users.noreply.github.com>
2025-01-22 13:38:06 +0200

    .

--- GalacticUniversity/GalacticUniversity.Models/Course.cs
+++ GalacticUniversity/GalacticUniversity.Models/Course.cs
@@ -15,7 +15,7 @@ namespace GalacticUniversity.Models
         public string CourseName { get; set; }
         public string Description { get; set; }
         public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime EndDate { get; set; } 
 
         // Foreign Keys
         [ForeignKey("Category")]
