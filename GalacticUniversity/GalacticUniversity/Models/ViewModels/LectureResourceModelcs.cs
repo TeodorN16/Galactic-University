939ccc788190375d44165b2b6e47adf4ca74ab56
TeodorN16 <nedelchevm.tm@gmail.com>
2025-02-04 14:42:49 +0200

    Fixed views

--- GalacticUniversity/GalacticUniversity/Models/ViewModels/LectureResourceModelcs.cs
+++ GalacticUniversity/GalacticUniversity/Models/ViewModels/LectureResourceModelcs.cs
@@ -1,6 +1,15 @@
-﻿namespace GalacticUniversity.Models.ViewModels
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GalacticUniversity.Models.ViewModels
 {
     public class LectureResourceModelcs
     {
+        public string? ResourceType { get; set; }
+
+        public string? ResourcePath { get; set; }
+
+        public int? LectureId { get; set; }
+        public Lecture Lecture { get; set; }
+
     }
 }
