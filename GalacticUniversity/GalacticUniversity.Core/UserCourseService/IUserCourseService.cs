bcd3df35ce9df201ba98a01fc8ecfec84b142e9a
TeodorN16 <149152387+TeodorN16@users.noreply.github.com>
2025-02-27 15:48:09 +0200

    Worked on Course logic

--- GalacticUniversity/GalacticUniversity.Core/UserCourseService/IUserCourseService.cs
+++ GalacticUniversity/GalacticUniversity.Core/UserCourseService/IUserCourseService.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GalacticUniversity.Core.Services;
+using GalacticUniversity.Models;
 
 namespace GalacticUniversity.Core.UserCourseService
 {
-    internal interface IUserCourseService
+    public interface IUserCourseService:IService<UserCourses>
     {
+
     }
 }
--- GalacticUniversity/GalacticUniversity.Core/UserCourseService/UserCourseService.cs
+++ GalacticUniversity/GalacticUniversity.Core/UserCourseService/UserCourseService.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GalacticUniversity.Core.Repository;
+using GalacticUniversity.Models;
 
 namespace GalacticUniversity.Core.UserCourseService
 {
-    internal class UserCourseService
+    internal class UserCourseService : IUserCourseService
     {
+        private readonly IRepository<UserCourses> _repo;
+
+        public UserCourseService(IRepository<UserCourses> repo)
+        { 
+            _repo = repo;
+        }
+        public async Task Add(UserCourses obj)
+        {
+           _repo.Add(obj);
+        }
+
+        public async Task Delete(UserCourses obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserCourses> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<UserCourses> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Update(UserCourses obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
