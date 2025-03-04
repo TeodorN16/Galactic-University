1b7d895ad3f5582cba3e04800e40e77d158d51f4
TeodorN16 <nedelchevm.tm@gmail.com>
2025-03-04 15:59:05 +0200

    Fixed Join Course implementation.
    Started adding User Profile View.

--- GalacticUniversity/GalacticUniversity/Controllers/CourseController.cs
+++ GalacticUniversity/GalacticUniversity/Controllers/CourseController.cs
@@ -170,8 +170,9 @@ namespace GalacticUniversity.Controllers
             await _courseService.Delete(await _courseService.Get(id));
             return RedirectToAction("Index");
         }
-        [HttpPost]
+        
         [Authorize(Roles ="User")]
+        [HttpGet]
         public async Task<IActionResult> JoinCourse(int courseId)
         {
             var userID = _userManager.GetUserId(User);
