3cb48a4a05bccb5b59899b2f2cbfb9e567539197
TeodorN16 <149152387+TeodorN16@users.noreply.github.com>
2025-03-21 15:45:03 +0200

    worked on design

---
+++ GalacticUniversity/GalacticUniversity/Controllers/PrivacyPolicyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GalacticUniversity.Controllers
+{
+    public class PrivacyPolicyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GalacticUniversity/GalacticUniversity/Controllers/TermsOfServiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GalacticUniversity.Controllers
+{
+    public class TermsOfServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
